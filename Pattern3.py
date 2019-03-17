a9b4f93ef73016810e65ca4fc2f1245fa2b1f783
Hello-Raviraj <32117138+Hello-Raviraj@users.noreply.github.com>
2019-03-17 20:25:43 +0530

    Pattern3.py

--- Pattern3.py
+++ Pattern3.py
@@ -1,9 +1,10 @@
-for i in range(5):
-  for n in range(i+1):
-    print(" ",end="")
-  for x in range(5-i):
-    print("*")
+for i in range(5):        #for row iteration
+  for n in range(i+1):    #for creating pattern of spaces like pattern2.instead of *,jst put an space.
+    print(" ",end="")  
+  for x in range(5-i):    #for creating actual pattern
+    print("*",end="")
   print()
   
   
-  #The idea behind this pattern is first create a pattern
+  #The idea behind this pattern is first create a pattern2(check in my repository) with spaces
+  #and after that take another loop for printing actual pattern.
