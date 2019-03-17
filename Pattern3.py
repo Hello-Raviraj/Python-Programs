8ac745d743f5cb1e165c4615d3cf0fe3533e34bb
Hello-Raviraj <32117138+Hello-Raviraj@users.noreply.github.com>
2019-03-17 20:20:51 +0530

    Pattern3.py

---
+++ Pattern3.py
@@ -0,0 +1,9 @@
+for i in range(5):
+  for n in range(i+1):
+    print(" ",end="")
+  for x in range(5-i):
+    print("*")
+  print()
+  
+  
+  #The idea behind this pattern is first create a pattern
