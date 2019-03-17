dd93798a9326103abc197af3ea9a2753ec0eee1c
Hello-Raviraj <32117138+Hello-Raviraj@users.noreply.github.com>
2019-03-17 20:44:06 +0530

    Pattern4.py

--- Pattern4.py
+++ Pattern4.py
@@ -1,6 +1,6 @@
-for i in range(5):
-  for n in range(5-i):
+for i in range(5):            #for row iteration
+  for n in range(5-i):        #for creating pattern of spaces like pattern1.instead of *,jst put an space.
     print(" ",end="")
-  for x in range(i+1):
+  for x in range(i+1):        #for creating actual pattern.
     print("*",end="")
-  print()
+  print()                     #for new line 
