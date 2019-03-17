983e89f051804c185b6a8aceaf00240e1b4013cc
Hello-Raviraj <32117138+Hello-Raviraj@users.noreply.github.com>
2019-03-17 20:11:59 +0530

    Pattern2.py

---
+++ Pattern2.py
@@ -0,0 +1,4 @@
+for i in range(5):            #row iteration
+  for n in range(i+1):         #column iteration
+    print("*",end="")         #end="" - so that after every iteration it wont go on next line
+  print()                     #for new line
