53ee3aa940e36fb3500bf73f2667b69277aaee89
Hello-Raviraj <32117138+Hello-Raviraj@users.noreply.github.com>
2019-03-17 21:26:23 +0530

    TrianglePattern2.py

---
+++ TrianglePattern2.py
@@ -0,0 +1,12 @@
+for i in range(5):
+  for n in range(5-i):
+    print(" ",end="")
+  for x in range(i+1):
+    print("* ",end="")   #just an extra space
+  print()
+  
+  
+  
+  #idea behind this is you just need an knowledge of one of the right angle pattern.
+  #Its all magic of an space.You can see in above program.
+  #its an program of an pattern4 from my repository
