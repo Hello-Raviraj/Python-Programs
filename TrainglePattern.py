395275318269e8767f44333107de2054ad79bcdd
Hello-Raviraj <32117138+Hello-Raviraj@users.noreply.github.com>
2019-03-17 21:21:05 +0530

    TrianglePattern.py

---
+++ TrainglePattern.py
@@ -0,0 +1,11 @@
+for i in range(5):
+  for n in range(i+1):
+    print(" ",end="")
+  for x in range(5-i):
+    print("* ",end="")          #just an extra space for traingle
+  print()
+  
+  
+  #idea behind this is you just need an knowledge of one of the right angle pattern.
+  #Its all magic of an space.You can see in above program.
+  #its an program of an pattern3
