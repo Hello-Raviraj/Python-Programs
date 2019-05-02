a279fb3cdbaa64f1e70e754ce986d115bbb6ae61
Hello-Raviraj <32117138+Hello-Raviraj@users.noreply.github.com>
2019-05-02 16:39:41 +0530

    Update duplicates.py

--- duplicates.py
+++ duplicates.py
@@ -5,5 +5,9 @@ for i in range(x):
     sea.append(y)
 
 print(sea)
+b=list(set(sea))                   #using list is another way as list dont contain duplicates
+print(b)
+
+
 sea=list(dict.fromkeys(sea))      #dictonary dont keep duplicate keys
 print(sea)
